8d264ec0a891873693bc6279237861bba819b884
akshaycj <akshaycj999@gmail.com>
2018-09-14 02:27:22 +0530

    Login/Signup UI fix

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,113 +1,213 @@
-import React, { Component } from 'react'
-import './Login.css';
-import a from '../pics/tradeZone.png';
-import { Input, Button, Icon } from 'antd';
-import '../SignUp/SignUp.css';
-import { Auth , db } from '../../config.js';
+import React, { Component } from "react";
+import "./Login.css";
+import a from "../pics/tradeZone.png";
+import { Input, Button, Icon } from "antd";
+import "../SignUp/SignUp.css";
+import { Auth, db } from "../../config.js";
 export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       login: false,
-      show : false,
-      showLogin : true ,
-      showSignUp : false ,
-      email:'',
-      password:'',
-      phone:'',
-      name : '' ,
-    }
+      show: false,
+      showLogin: true,
+      showSignUp: false,
+      email: "",
+      password: "",
+      phone: "",
+      name: ""
+    };
   }
-  
-  onName = (e) => {
-    this.setState({name : e.target.value})
-  }
-  
-  onPhone = (e) => {
-    this.setState({phone : e.target.value})
-  }
-  
-  onEmail = (e) => {
-    this.setState({email : e.target.value })
-  }
-  
-  onPassword = (e) => {
-    this.setState({password : e.target.value })
-  }
-  
+
+  onName = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  onPhone = e => {
+    this.setState({ phone: e.target.value });
+  };
+
+  onEmail = e => {
+    this.setState({ email: e.target.value });
+  };
+
+  onPassword = e => {
+    this.setState({ password: e.target.value });
+  };
+
   onSignUp = () => {
-    var that = this ;
-    this.setState({showSignUp : false})
-    Auth.createUserWithEmailAndPassword(this.state.email , this.state.password).then(function(object){
+    var that = this;
+    this.setState({ showSignUp: false });
+    Auth.createUserWithEmailAndPassword(
+      this.state.email,
+      this.state.password
+    ).then(function(object) {
       var d = {
-        name : that.state.name ,
-        email : that.state.email ,
-        phone : that.state.phone ,
-      }
-      db.ref('users').child(object.user.uid).set(d);
-    })
-  }
-  
+        name: that.state.name,
+        email: that.state.email,
+        phone: that.state.phone
+      };
+      db.ref("users")
+        .child(object.user.uid)
+        .set(d);
+    });
+  };
+
   onLogin = () => {
     var that = this;
-    Auth.signInWithEmailAndPassword(this.state.email , this.state.password).catch(function(error){    
-    }).then(function(user){
-      if(user){
-        console.log("hi",user.user.uid);
-        that.setState({login:true , showLogin : false})
-      }
-    })
-  }
-  
+    Auth.signInWithEmailAndPassword(this.state.email, this.state.password)
+      .catch(function(error) {})
+      .then(function(user) {
+        if (user) {
+          console.log("hi", user.user.uid);
+          that.setState({ login: true, showLogin: false });
+        }
+      });
+  };
+
   closeLogin() {
-    this.setState({showLogin : false})
-    this.props.value(this.state.showLogin)
+    this.setState({ showLogin: false });
+    this.props.value(false);
   }
-  
+
   showsignup() {
-    this.setState({ showSignUp : true , showLogin : false})
+    this.setState({ showLogin: false });
   }
-  
+
   render() {
     return (
       <div>
-        {this.state.showLogin ?
-          <div className='backgroundlogin'>
+        <div className="backgroundlogin">
+          {this.state.showLogin ? (
             <div className="login">
-              <Icon type="close-circle" theme="outlined" onClick={this.closeLogin.bind(this)} style={{ cursor: 'pointer',alignSelf: 'flex-end', fontSize: '15px', padding: '10px', position: 'absolute' }} />
-              <div className='outers'>
+              <Icon
+                type="close-circle"
+                theme="outlined"
+                onClick={this.closeLogin.bind(this)}
+                style={{
+                  cursor: "pointer",
+                  alignSelf: "flex-end",
+                  fontSize: "25px",
+                  padding: "10px",
+                  position: "absolute"
+                }}
+              />
+              <div className="outers">
                 <h1 style={{ color: "rgba(250,125,150)" }}>Login</h1>
-                <img src={a} style={{ width: '65%', margin: 'auto', borderRadius: '90px', padding: '0' }} />
-                <span style={{ margin: 'auto', display: 'flex', flexDirection: 'column', height: '20%', justifyContent: 'space-evenly' }}>
-                  <Input placeholder='Email' style={{ width: '90%', margin: 'auto', borderRadius: 15 }} onChange={this.onEmail}/>
-                  <Input placeholder='Password' style={{ width: '90%', margin: 'auto', borderRadius: 15 }} onChange={this.onPassword}/>
+                <img
+                  src={a}
+                  style={{
+                    width: "65%",
+                    margin: "auto",
+                    borderRadius: "90px",
+                    padding: "0"
+                  }}
+                />
+                <div
+                  style={{
+                    margin: "auto",
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "20%",
+                    width: "100%",
+                    justifyContent: "space-evenly"
+                  }}
+                >
+                  <Input
+                    placeholder="Email"
+                    style={{ width: "90%", margin: "auto", borderRadius: 15 }}
+                    onChange={this.onEmail}
+                  />
+                  <Input
+                    placeholder="Password"
+                    style={{ width: "90%", margin: "auto", borderRadius: 15 }}
+                    onChange={this.onPassword}
+                  />
+                </div>
+                <div
+                  className="common-button app-primary-dark"
+                  style={{ width: "40%", margin: "auto", borderRadius: 15 }}
+                  onClick={this.onLogin}
+                >
+                  Login
+                </div>
+                <div
+                  onClick={this.showsignup.bind(this)}
+                  style={{ cursor: "pointer" }}
+                >
+                  Not a user? SignUp
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="signup">
+              <Icon
+                type="close-circle"
+                theme="outlined"
+                onClick={this.closeLogin.bind(this)}
+                style={{
+                  cursor: "pointer",
+                  alignSelf: "flex-end",
+                  fontSize: "15px",
+                  padding: "10px",
+                  position: "absolute"
+                }}
+              />
+              <div className="outersofsignup">
+                <h1 style={{ color: "rgba(250,125,150)" }}>SignUp</h1>
+                <img
+                  src={a}
+                  style={{
+                    width: "60%",
+                    margin: "auto",
+                    borderRadius: "90px",
+                    padding: "0"
+                  }}
+                />
+                <span
+                  style={{
+                    margin: "auto",
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "40%",
+                    justifyContent: "space-evenly",
+
+                    width: "100%"
+                  }}
+                >
+                  <Input
+                    placeholder="Name"
+                    style={{ width: "90%", margin: "auto", borderRadius: 15 }}
+                    onChange={this.onName}
+                  />
+                  <Input
+                    placeholder="Phone"
+                    style={{ width: "90%", margin: "auto", borderRadius: 15 }}
+                    onChange={this.onPhone}
+                  />
+                  <Input
+                    placeholder="Email"
+                    style={{ width: "90%", margin: "auto", borderRadius: 15 }}
+                    onChange={this.onEmail}
+                  />
+                  <Input
+                    placeholder="Password"
+                    style={{ width: "90%", margin: "auto", borderRadius: 15 }}
+                    onChange={this.onPassword}
+                  />
                 </span>
-                <Button type='primary'  style={{ width: '40%', margin: 'auto', borderRadius: 15 }} onClick={this.onLogin}>Login</Button>
-              <div onClick={this.showsignup.bind(this)}>Not a user?SignUp</div>  
-         </div>
+                <div
+                  className="common-button app-primary-dark"
+                  style={{ width: "40%", margin: "auto", borderRadius: 15 }}
+                  onClick={this.onSignUp}
+                >
+                  SignUp
+                </div>
+              </div>
             </div>
-          </div>
-          : null }
-          {this.state.showSignUp ?
-          <div className='backgroundlogin'>
-          <div className="signup">
-              <Icon type="close-circle" theme="outlined" onClick={this.closeLogin.bind(this)} style={{ cursor: 'pointer',alignSelf: 'flex-end', fontSize: '15px', padding: '10px', position: 'absolute' }} />
-            <div className='outersofsignup'>
-              <h1 style={{ color: "rgba(250,125,150)" }}>SignUp</h1>
-              <img src={a} style={{ width: '60%', margin: 'auto', borderRadius: '90px', padding: '0' }} />
-              <span style={{ margin: 'auto', display: 'flex', flexDirection: 'column', height: '40%', justifyContent: 'space-evenly' }}>
-                <Input placeholder='Name' style={{ width: '90%', margin: 'auto', borderRadius: 15 }} onChange={this.onName}/>
-                <Input placeholder='Phone' style={{ width: '90%', margin: 'auto', borderRadius: 15 }} onChange={this.onPhone}/>
-                <Input placeholder='Email' style={{ width: '90%', margin: 'auto', borderRadius: 15 }} onChange={this.onEmail}/>
-                <Input placeholder='Password' style={{ width: '90%', margin: 'auto', borderRadius: 15 }} onChange={this.onPassword}/>
-              </span>
-              <Button type='primary' style={{ width: '40%', margin: 'auto', borderRadius: 15 }} onClick={this.onSignUp}>SignUp</Button>
-            </div>
-          </div>
-          </div>
-          : null
-          }
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
