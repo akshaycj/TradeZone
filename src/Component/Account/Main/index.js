0aa2eb91866d352d805318433f071c5380e0e2a4
akshaycj <akshaycj999@gmail.com>
2018-09-30 22:37:23 +0530

    Fixed width for nested route container

--- src/Component/Account/Main/index.js
+++ src/Component/Account/Main/index.js
@@ -24,7 +24,7 @@ export default class extends Component {
             </Menu>
           </div>
         </div>
-        <div className="details">{this.props.children}</div>
+        <div className="mdetails">{this.props.children}</div>
       </div>
     );
   }
