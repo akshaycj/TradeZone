54422107f51b1e95ca8e72bf2f5e3099265dfc46
akshaycj <akshaycj999@gmail.com>
2018-09-30 21:55:28 +0530

    Added Profile Menu Items

--- src/Component/Account/index.js
+++ src/Component/Account/index.js
@@ -3,14 +3,25 @@ import "./index.css";
 
 import { Menu, Icon } from "antd";
 
+const MenuItem = Menu.Item;
+
 export default class extends Component {
   render() {
     return (
       <div>
         <div className="account-profile-container">
           <div className="avatar">.</div>
-          <div className="menu">
-            <Menu mode="inline" style={{ width: "100%" }} />
+          <div className="menu" style={{ width: "100%" }}>
+            <Menu
+              mode="inline"
+              style={{ width: "100%", borderRight: 0 }}
+              defaultSelectedKeys="1"
+            >
+              <MenuItem key="1">Overview</MenuItem>
+              <MenuItem key="2">Add Product</MenuItem>
+              <MenuItem key="3">Manage Products</MenuItem>
+              <MenuItem key="4">Current Plan</MenuItem>
+            </Menu>
           </div>
         </div>
       </div>
