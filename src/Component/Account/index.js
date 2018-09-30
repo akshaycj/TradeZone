c0f3e59d4421c4127a6b887bcdd9a93148f7e956
akshaycj <akshaycj999@gmail.com>
2018-09-30 21:46:50 +0530

    Added menu layout

--- src/Component/Account/index.js
+++ src/Component/Account/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./index.css";
 
+import { Menu, Icon } from "antd";
+
 export default class extends Component {
   render() {
     return (
       <div>
         <div className="account-profile-container">
           <div className="avatar">.</div>
+          <div className="menu">
+            <Menu mode="inline" style={{ width: "100%" }} />
+          </div>
         </div>
       </div>
     );
