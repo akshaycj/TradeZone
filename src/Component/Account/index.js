53e19d49dd9d85cc0431fdd76dc78357ce50d2e2
akshaycj <akshaycj999@gmail.com>
2018-09-30 22:17:59 +0530

    Added nested router for Account

--- src/Component/Account/index.js
+++ src/Component/Account/index.js
@@ -1,30 +1,11 @@
 import React, { Component } from "react";
-import "./index.css";
-
-import { Menu, Icon } from "antd";
-
-const MenuItem = Menu.Item;
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./Main";
+import AddProduct from "../Addproduct/Addproduct";
+import OverView from "./Overview";
 
 export default class extends Component {
   render() {
-    return (
-      <div>
-        <div className="account-profile-container">
-          <div className="avatar">.</div>
-          <div className="menu" style={{ width: "100%" }}>
-            <Menu
-              mode="inline"
-              style={{ width: "100%", borderRight: 0 }}
-              defaultSelectedKeys="1"
-            >
-              <MenuItem key="1">Overview</MenuItem>
-              <MenuItem key="2">Add Product</MenuItem>
-              <MenuItem key="3">Manage Products</MenuItem>
-              <MenuItem key="4">Current Plan</MenuItem>
-            </Menu>
-          </div>
-        </div>
-      </div>
-    );
+    return <div />;
   }
 }
