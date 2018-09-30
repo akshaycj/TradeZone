9a4095ecbf54145b5926def065979f1d31ee33d9
akshaycj <akshaycj999@gmail.com>
2018-09-30 21:25:25 +0530

    Added Profile Layout Container

--- src/Component/Account/index.js
+++ src/Component/Account/index.js
@@ -3,6 +3,12 @@ import "./index.css";
 
 export default class extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <div className="account-profile-container">
+          <div className="avatar">.</div>
+        </div>
+      </div>
+    );
   }
 }
