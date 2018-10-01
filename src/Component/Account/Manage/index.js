d5a7fc5c0b2441cd83fe2ee5064e5ea5764dd654
akshaycj <akshaycj999@gmail.com>
2018-10-01 11:24:43 +0530

    Added product fetching to manage

--- src/Component/Account/Manage/index.js
+++ src/Component/Account/Manage/index.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 import "./index.css";
+import { db } from "../../../config";
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+  componentDidMount() {
+    db.ref("users")
+      .child("aYjX8BD4w5SADTlnTQB3pl13HKJ2")
+      .on(
+        "value",
+        function(dataSnap) {
+          var data = [];
+          dataSnap.forEach(element => {
+            data.push(element.val());
+          });
+
+          this.setState({ products: data });
+        }.bind(this)
+      );
+  }
   render() {
     return <div />;
   }
