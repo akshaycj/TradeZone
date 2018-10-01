dc5e6675c69f359f378e7901b89dbc0319a3c771
akshaycj <akshaycj999@gmail.com>
2018-10-01 17:41:25 +0530

    fixed alignment in profile page

--- src/Component/Account/Profile/index.js
+++ src/Component/Account/Profile/index.js
@@ -1,68 +1,88 @@
-import React, { Component } from 'react'
-import "./index.css"
-import {Input,Icon,Button} from "antd"
+import React, { Component } from "react";
+import "./index.css";
+import { Input, Icon, Button } from "antd";
 
 export default class index extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          name: "sam",
-          company: "GOT",
-          email: "winteriscoming@gmail.com",
-          phone: "9444333333"
-        };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "sam",
+      company: "GOT",
+      email: "winteriscoming@gmail.com",
+      phone: "9444333333"
+    };
+  }
   render() {
     return (
-      <div style={{display:"flex",flexDirection:"column",margin:"auto",justifyContent:"center"}}>
-      <h1>PROFILE</h1>
-         <Input
-              style={{ width: "360px", margin: "10px" }}
-              addonBefore={
-                <div>
-                  <Icon
-                    style={{ fontSize: "23px" }}
-                    type="user"
-                    theme="outlined"
-                  />
-                  Name
-                </div>
-              }
-              defaultValue={this.state.name}
-            />
-            <Input
-              style={{ width: "360px", margin: "10px" }}
-              addonBefore={
-                <div>
-                  <Icon
-                    style={{ fontSize: "23px" }}
-                    type="mail"
-                    theme="outlined"
-                  />
-                  Email
-                </div>
-              }
-              defaultValue={this.state.email}
-            />
-            <Input
-              style={{ width: "360px", margin: "10px" }}
-              addonBefore={
-                <div>
-                  <Icon
-                    style={{ fontSize: "23px" }}
-                    type="mobile"
-                    theme="outlined"
-                  />
-                  Phone
-                </div>
-              }
-              defaultValue={this.state.phone}
-            />
-            <Button style={{width:"200px",margin:"auto"}}>Update  </Button>
-            
-        
-          </div>
-    
-    )
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "auto",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <h1>Your Profile</h1>
+        <Input
+          style={{ width: "360px", margin: "10px" }}
+          addonBefore={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Icon style={{ fontSize: "23px" }} type="user" theme="outlined" />
+              Name
+            </div>
+          }
+          defaultValue={this.state.name}
+        />
+        <Input
+          style={{ width: "360px", margin: "10px" }}
+          addonBefore={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Icon style={{ fontSize: "23px" }} type="mail" theme="outlined" />
+              Email
+            </div>
+          }
+          defaultValue={this.state.email}
+        />
+        <Input
+          style={{ width: "360px", margin: "10px" }}
+          addonBefore={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Icon
+                style={{ fontSize: "23px" }}
+                type="mobile"
+                theme="outlined"
+              />
+              Phone
+            </div>
+          }
+          defaultValue={this.state.phone}
+        />
+        <div
+          className="common-button app-accent"
+          style={{ width: "100px", margin: "auto" }}
+        >
+          Update{" "}
+        </div>
+      </div>
+    );
   }
 }
