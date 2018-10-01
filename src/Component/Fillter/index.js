c9323d5828aca02c2b29b114ed2acaed409ca3e2
akshaycj <akshaycj999@gmail.com>
2018-10-01 22:21:49 +0530

    search page products added

--- src/Component/Fillter/index.js
+++ src/Component/Fillter/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import "./index.css";
+import Product from "../Home/Recents/Recent";
 
 export default class extends Component {
   render() {
+    const items = [];
+    for (let i = 0; i < 12; i++) {
+      items.push(
+        <div style={{ marginTop: 2 }}>
+          <Product />
+        </div>
+      );
+    }
     return (
       <div className="filter-main-container">
         <div className="sort-container">.</div>
-        <div className="product-container">.</div>
+        <div className="product-container">{items}</div>
       </div>
     );
   }
