63e5b1c861af7264938009d543f11bf69c1d415a
sachinnair26 <sachin.nair32@gmail.com>
2019-01-18 09:03:17 +0530

    now

--- src/Component/Account/Overview/Overview.js
+++ src/Component/Account/Overview/Overview.js
@@ -51,18 +51,61 @@ import {connect } from 'react-redux';
       <div>
       {this.state.spin ? <div style={{display:'flex',margin:'auto',height:'100vh',justifyContent:'center',alignItems:'center'}}>
         <Spin/>
-        </div> : <div style={{display:'flex',flexDirection:'column',justifyContent:"space-between",height:'60vh'}}>
-        <div>Name:{this.state.name}</div>
-        <div>Email:{this.state.email}</div>
-        <div>CompanyName:{this.state.companyName}</div>
-        <div>Phone:{this.state.phone}</div>
-        <div>AboutCompany:{this.state.aboutCompany}</div>
-        <div>No of staff:{this.state.staffNo}</div>
-        <div>Company Address:{this.state.companyAddr}</div>
-        <div>VatNo:{this.state.vatNo}</div>
-        <div>Location:{this.state.location}</div>
-        <div>LiscenceNo:{this.state.liscenceNo}</div>
-        <div>YearOf Establishment:{this.state.yearOfEstab}</div>
+        </div> : <div style={{display:'flex',flexDirection:'column',alignItems:'flex-start',justifyContent:"flex-start",height:'60vh'}}>
+                <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>Name:</span>
+             <span style={{fontSize:'20px',fontWeight:'600'}}> {this.state.name}</span>
+            </div>
+        <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>Email:</span>
+             <span style={{fontSize:'20px',fontWeight:'600'}}> {this.state.email}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>
+                CompanyName:
+              </span>
+              <span style={{fontSize:'20px',fontWeight:'600'}}>{this.state.companyName}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>Phone:</span>
+              <span style={{fontSize:'20px',fontWeight:'600'}}>{this.state.phone}</span>
+            </div>
+          
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>LiscenceNo:</span>
+             <span style={{fontSize:'20px',fontWeight:'600'}}> {this.state.liscenceNo}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>No of staff:</span>
+             <span style={{fontSize:'20px',fontWeight:'600'}}> {this.state.staffNo}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>Vat No:</span>
+             <span style={{fontSize:'20px',fontWeight:'600'}}> {this.state.vatNo}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>Year of Establishment:</span>
+             <span style={{fontSize:'20px',fontWeight:'600'}}> {this.state.yearOfEstab}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>
+                Location:
+              </span>
+              <span style={{fontSize:'20px',fontWeight:'600'}}>{this.state.location}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>
+                Company Address:
+              </span>
+              <span style={{fontSize:'20px',fontWeight:'600'}}>{this.state.companyAddr}</span>
+            </div>
+            <div>
+              <span style={{ fontSize: '18px', fontWeight: 500 }}>
+                AboutCompany:
+              </span>
+              <span style={{fontSize:'20px',fontWeight:'600'}}>{this.state.aboutCompany}</span>
+            </div>
+
         </div>}
       </div>
     )
