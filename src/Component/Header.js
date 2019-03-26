5673c64092a4ce5fe855959d9c79abdd3a4e051a
sachinnair26 <sachin.nair32@gmail.com>
2019-03-26 08:37:33 +0530

    hamburger menu

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -152,66 +152,21 @@ class Header extends React.Component {
         </Menu.Item>
       </Menu>
     );
-    const menuforham = (
+    
+    const loggedInMenu = (
       <Menu mode="vertical" style={{ backgroundColor: '#f7f7f7', border: 0 }}>
-        <SubMenu
-          key="sub1"
-          title={
-            <span>
-              <Icon type="mail" />
-              <span>Navigation One</span>
-            </span>
-          }
-        >
-          <MenuItemGroup title="Item 1">
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
-          </MenuItemGroup>
-          <MenuItemGroup title="Iteom 2">
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
-          </MenuItemGroup>
-        </SubMenu>
-        <SubMenu
-          key="sub2"
-          title={
-            <span>
-              <Icon type="appstore" />
-              <span>Navigation Two</span>
-            </span>
-          }
-        >
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-        </SubMenu>
-        <SubMenu
-          key="sub4"
-          title={
-            <span>
-              <Icon type="setting" />
-              <span>Navigation Three</span>
-            </span>
-          }
-        >
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
-        </SubMenu>
-        <SubMenu key="country" title="Country">
-          <Menu.Item key="7">Option 7</Menu.Item>
-          <Menu.Item key="8">Option 8</Menu.Item>
-          <Menu.Item key="7">Option 9</Menu.Item>
-        </SubMenu>
-        <SubMenu key="category" title="category">
-          <Menu.Item key="7">Option 7</Menu.Item>
-          <Menu.Item key="8">Option 8</Menu.Item>
-          <Menu.Item key="7">Option 9</Menu.Item>
-        </SubMenu>
+      <Menu.Item>
+        {this.state.type === 'seller' ? (
+          <Link to="/account">Account</Link>
+        ) : null}
+      </Menu.Item>
+      <Menu.Item onClick={this.signUserOut.bind(this)}>SignOut</Menu.Item>
+      </Menu>
+    );
+    const notLoggedInMenu = (
+      <Menu mode="vertical" style={{ backgroundColor: '#f7f7f7', border: 0 }}>
+      <Menu.Item  onClick={this.getlogin.bind(this)}>User</Menu.Item>
+      <Menu.Item    onClick={this.getSellerLogin.bind(this)}>Seller</Menu.Item>
       </Menu>
     );
     const userDropdownMenu = (
@@ -354,7 +309,7 @@ class Header extends React.Component {
               <Icon type="close" style={{marign:10}} onClick={this.showSearchFun}/>
                 </div>
                :  <div className='app-primary header-resp'>
-                  <Dropdown overlay={menuforham}>
+                  <Dropdown overlay={this.state.loggedin ? loggedInMenu :notLoggedInMenu}>
             <div className="hamburger">
               <span className="hamSpan" />
               <span className="hamSpan" />
@@ -371,7 +326,7 @@ class Header extends React.Component {
                         </div>
                 <img src={search} className='resp-search-icon' onClick={this.showSearchFun} />
            </div>
-                        }
+                  }
               
               </div>
       </div>
