f4fa67d684912e3f09a72399bdf194410b3ba4e9
Adhil Juvane <adhilmp3@gmail.com>
2019-07-08 00:47:46 +0530

    address displayed completly and neatly

--- src/Component/Home/FourthComponent.js
+++ src/Component/Home/FourthComponent.js
@@ -1,24 +1,38 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import './FourthComponent.css';
+import { LoginAction } from "../Actions/Login";
+import { connect } from "react-redux";
 import { Input } from 'antd';
 
-export default class FourthComponent extends Component{
-  render(){
-    return(
+class FourthComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  onLogin = () => {
+    this.props.LoginAction(this.state.email, this.state.password);
+  }
+
+  render() {
+    return (
       <div className="mainDivInF4">
         <div className="fourth1">
-            <p className="textIn4th1">Your<br/> Personalized Recommendations here</p>
+          <p className="textIn4th1">Your<br /> Personalized Recommendations here</p>
         </div>
         <div className="fourth2">
           <div className="div1Infourth2">
             <p>Sign in to see</p>
           </div>
           <div className="div2Infourth2">
-            <Input placeholder="Username"/>
-            <Input placeholder="Password"/>
+            <Input placeholder="Username" />
+            <Input placeholder="Password" />
           </div>
           <div className="div3Infourth2">
-            <p className="buttonIn4th2">Sign In</p>
+            <p className="buttonIn4th2" onClick={this.onLogin}>Sign In</p>
           </div>
         </div>
         <div className="fourth3">
@@ -28,3 +42,20 @@ export default class FourthComponent extends Component{
     );
   }
 }
+
+const mapActionToProps = {
+  LoginAction: LoginAction,
+}
+const mapStateToProps = state => (console.log(state),
+  {
+    authenticated: state.authenticated,
+    signup: state.signup,
+    err: state.err,
+    signuperr: state.signuperr,
+    user: state.user
+  }
+);
+export default connect(
+  mapStateToProps,
+  mapActionToProps
+)(FourthComponent);
