184091ac135471a3fa3ff245989121457f6f249f
Adhil Juvane <adhilmp3@gmail.com>
2019-07-10 14:12:33 +0530

    hover made proper

--- src/Component/Home/Block.js
+++ src/Component/Home/Block.js
@@ -1,37 +1,36 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './Block.css';
-import {Link} from 'react-router-dom';
-export default class Block extends Component{
-    constructor(props){
+import { Link } from 'react-router-dom';
+export default class Block extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            link:""
+            link: ""
         }
     }
-    componentDidMount(){
-        
-        
+    componentDidMount() {
+
+
 
     }
-render(){
-    return(
+    render() {
+        return (
             <div>
-        
-           <div className='element'>
-                <img src={this.props.pic} alt="maga" style={{minWidth:'160px',maxHeight:'160px'}} />
-                <div className='overlay1'>
-                    <span className="detail">
-                        <h1>{this.props.name}</h1>
-                        {this.props.text} 
-                    </span>
-                    <span className="price"> 
-                        <h6>AED</h6>
-                        <h2>{this.props.price}</h2>
-                    </span>
-                </div>
-           </div>
-        </div>
 
-    )
-}
+                <div className='element'>
+                    <img src={this.props.pic} alt="maga" style={{ minWidth: '160px', maxHeight: '160px' }} />
+                    <div className='overlay1'>
+                        <span className="detail">
+                            <h1>{this.props.name}</h1>
+                            {this.props.text}
+                        </span>
+                        <span className="detail">
+                            <h2>{this.props.price}</h2>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
 }
