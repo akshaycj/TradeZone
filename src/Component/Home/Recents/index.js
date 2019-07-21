3149d8f4795ccd323bcaf3818c8dc31f284e50da
Adhil Juvane <adhilmp3@gmail.com>
2019-07-21 12:58:25 +0530

    recent products order fixed

--- src/Component/Home/Recents/index.js
+++ src/Component/Home/Recents/index.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import "./index.css";
 import Recent from "./Recent";
 import { db } from '../../../config';
-import { Carousel } from "react-responsive-carousel";
-
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default class extends Component {
   constructor(props) {
@@ -20,11 +17,9 @@ export default class extends Component {
       limitToLast(5).on("value", function (data) {
         data.forEach(i => {
           thus.state.recent.push({ data: i.val(), key: i.key })
-
         })
         thus.setState({ spin: true })
       })
-
   }
 
   render() {
@@ -33,8 +28,7 @@ export default class extends Component {
     return (
       <div >
         {this.state.spin ? <div className="recents">
-          {this.state.recent.map((o, index) => (
-
+          {this.state.recent.reverse().map((o, index) => (
             <Recent value={o} key={index} />
           ))}
         </div> : null}
